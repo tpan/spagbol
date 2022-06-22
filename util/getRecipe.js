d3e70503b012e2f4134dac0da00bc55d66185a76
Thomas Pan <thomastpan@gmail.com>
2022-06-21 22:37:14 -0700

    MVP with supabase query

--- util/getRecipe.js
+++ util/getRecipe.js
@@ -1,6 +1,15 @@
+import { createClient } from "@supabase/supabase-js"
+
+const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } = process.env
+
+const supaBase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
 export async function getRecipe() {
-    return { 
-        name: "Spaghetti Bolegnese",
-        ingredients: ["pasta", "I have no idea lol"]
-}
+    const rows = await supaBase.from('recipes').select('*')
+    const length = rows.data.length;
+    const maxIndex = length -1;
+    const randomIndex = Math.round(Math.random() * maxIndex);
+    const row = rows.data[randomIndex];
+
+    return row
 }
