06a9895348d3cf2d2e11037b002b49756cb0b0af
Thomas Pan <thomastpan@gmail.com>
2022-06-22 14:28:08 -0700

    It does the thing!

--- pages/api/generate-recipe.js
+++ pages/api/generate-recipe.js
@@ -1,9 +1,56 @@
-import OpenAI from 'openai';
-
+const { Configuration, OpenAIApi } = require("openai");
+import { supabaseClient } from '../../util/supabaseClient'
 
 export default async function generateRecipe(req, res) {
     if (req.method === "POST") {
-        console.log(req)
+        const configuration = new Configuration({
+            organization: "org-Z5erv9yk6lMCpQzcAWzWgitA",
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+
+        let {ingredients} = req.body;
+
+        ingredients = ingredients ?? [];
+
+        const openai = new OpenAIApi(configuration);
+        const prompt = `Write a recipe based on these ingredients:\n\nIngredients:\n${ingredients.join('\n')}`
+
+        try {
+            const completion = await openai.createCompletion({
+                model: "text-davinci-002",
+                prompt: prompt,
+                temperature: 0,
+                max_tokens: 500,
+            });
+
+            if (completion.data.choices && completion.data.choices.length > 0) {
+                const choice = completion.data.choices[0];
+                let values = choice.text.split('\n');
+                values = values.filter(v => v.trim() != '');
+                const name = values[0];
+                const instructions = values.slice(1, values.length - 1);
+                const { data, error } = await supabaseClient
+                  .from('recipes')
+                  .insert([
+                    {
+                      name: name,
+                      image_url: null,
+                      ingredients: ingredients,
+                      instructions: instructions,
+                      source: 'openAI Generated'
+                    }
+                  ]);
+                console.log(error)
+          
+                return res.status(200).json((data || [])[0]);
+            }
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.status);
+                console.log(error.response.data);
+              } else {
+                console.log(error.message);
+              }
+        }
     }
-    return console.log(req)
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,14 +7,13 @@ const Home = (props) => {
 
   const { recipe } = props
   const [ingredientText, setIngredientText] = useState('');
+  const [recipeString, setRecipeString] = useState(JSON.stringify(recipe, null, 2));
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-
-
-  const recipeString = JSON.stringify(recipe, null, 2)
+  
   return (
     <div className="flex min-h-screen flex-col justify-center py-2">
-      <main className="flex w-full flex-1 flex-col justify-center mx-auto text-center max-w-md">
+      <main className="flex w-full flex-1 flex-col justify-center mx-auto text-center max-w-screen-md">
         <h1 className="text-4xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://github.com/tpan/spagbol">
@@ -24,17 +23,16 @@ const Home = (props) => {
         <p className='mt-2'>Enter a few ingredients and I'll tell you what's for dinner! 👨🏻‍🍳</p>
       
       <div className="mt-5 flex justify-between">
-        <Link href="/api">
-          <a className="underline font-bold">Developer API /api</a>
-        </Link>
-
+        <p className="underline font-bold">
+        </p>
       </div>
+
       <div className="mt-4 text-left rounded-lg bg-slate-600 text-white p-5">
-        <pre>
+        <pre className="w-10">
           {recipeString}
         </pre>
       </div>
-      <textarea className='border border-solid border-black rounded-xl outline-none p-4 w-4/5' style={{ minHeight: 120 }}
+      <textarea className='border border-solid border-black rounded-xl outline-none p-5' style={{ minHeight: 120 }}
             rows={1 + (ingredientText.split('\n').length)}
             value={ingredientText} onChange={(evt) => setIngredientText(evt.target.value)}
             placeholder={'Strawberries\nPasta\nCilantro'}>
@@ -53,6 +51,9 @@ const Home = (props) => {
                 axios.post('/api/generate-recipe', { ingredients })
                   .then(response => {
                     setLoading(false);
+                    console.log(recipeString)
+                    setRecipeString(JSON.stringify(response.data, null, 2))
+                    console.log(recipeString)
                     setErrorMessage('');
                   })
                   .catch(error => {
@@ -72,11 +73,10 @@ const Home = (props) => {
 
 export async function getServerSideProps() {
   const recipe = await getRecipe();
-  // const ingredients = 
 
   return {
     props: {
-      // recipe
+      recipe
     }
   }
 }
